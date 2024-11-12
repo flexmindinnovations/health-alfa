4e7e11b4ba279e42394f9b3b4c517d12d4b8e3b7
shafiqWork <cricketdream2k@gmail.com>
2024-11-12 15:31:24 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import './App.css';
 import { Route, Routes, Navigate } from "react-router-dom";
 import { Layout } from "./pages/layout.jsx";
-import { Home } from "./pages/home.jsx";
+import { Home } from "./pages/dashboard/home.jsx";
 import PageNotFound from "./pages/page-not-found.jsx";
 import { Login } from "./pages/login.jsx";
 import { Register } from "./pages/register.jsx";
-import { UserOrders } from "./pages/orders.jsx";
+import { AboutUS } from "./pages/dashboard/AboutUS.jsx";
 
 function App() {
     return (
         <Routes>
             <Route path="/app" element={<Layout />}>
                 <Route index element={<Home />} />
-                <Route path="users" element={<UserOrders />} />
-                <Route path="documents" element={<UserOrders />} />
-                <Route path="medical-tests" element={<UserOrders />} />
-                <Route path="health-conditions" element={<UserOrders />} />
-                <Route path="medications" element={<UserOrders />} />
-                <Route path="allergies" element={<UserOrders />} />
-                <Route path="immunizations" element={<UserOrders />} />
-                <Route path="orders" element={<UserOrders />} />
+                <Route path="users" element={<AboutUS />} />
+                <Route path="documents" element={<AboutUS />} />
+                <Route path="medical-tests" element={<AboutUS />} />
+                <Route path="health-conditions" element={<AboutUS />} />
+                <Route path="medications" element={<AboutUS />} />
+                <Route path="allergies" element={<AboutUS />} />
+                <Route path="immunizations" element={<AboutUS />} />
+                <Route path="orders" element={<AboutUS />} />
             </Route>
 
             <Route path="/login" element={<Login />} />
---
+++ src/pages/dashboard/AboutUs.jsx
@@ -0,0 +1,8 @@
+export function AboutUS() {
+
+    return (
+        <div className="about-page">
+            <p>AboutUS Page</p>
+        </div>
+    )
+}
