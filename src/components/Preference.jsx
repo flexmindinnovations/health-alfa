349c8e88172f7cff704f0733d9f9c26d18ac371f
Imran <flexmindinnovations@outlook.com>
2024-11-18 10:15:18 +0530

    Update Preference.jsx

--- src/components/Preference.jsx
+++ src/components/Preference.jsx
@@ -1,118 +1,154 @@
-import {Container, Grid, Group, Loader, Select, Text, useDirection, useMantineColorScheme} from '@mantine/core'
-import {useTranslation} from 'react-i18next'
-import {forwardRef, useEffect, useState} from 'react'
-import {MonitorDot, MoonIcon, Sun} from 'lucide-react'
+import {
+  Container,
+  Grid,
+  Group,
+  Loader,
+  Select,
+  Text,
+  useDirection,
+  useMantineColorScheme,
+  Center,
+  SegmentedControl,
+  rem
+} from '@mantine/core'
+import { useTranslation } from 'react-i18next'
+import { forwardRef, useEffect, useState, createElement } from 'react'
+import { MonitorDot, MoonIcon, Sun } from 'lucide-react'
 
-const providedLanguages = [{id: 1, label: 'English', value: 'en'}, {id: 2, label: 'Arabic', value: 'ar'}]
+const providedLanguages = [
+  { id: 1, label: 'English', value: 'en' },
+  { id: 2, label: 'Arabic', value: 'ar' }
+]
 
-const themeData = [{value: 'auto', label: 'Auto', icon: MonitorDot}, {
+const themeData = [
+  { value: 'auto', label: 'Auto', icon: MonitorDot },
+  {
     value: 'light',
     label: 'Light',
     icon: Sun
-}, {value: 'dark', label: 'Dark', icon: MoonIcon}]
+  },
+  { value: 'dark', label: 'Dark', icon: MoonIcon }
+]
 
 // eslint-disable-next-line react/display-name
-const SelectItem = forwardRef(({label, icon, ...others}, ref) => (<div ref={ref} {...others}>
+const SelectItem = forwardRef(({ label, icon, ...others }, ref) => (
+  <div ref={ref} {...others}>
     {' '}
     <Group noWrap>
-        {' '}
-        {icon} <Text>{label}</Text>{' '}
+      {' '}
+      {icon} <Text>{label}</Text>{' '}
     </Group>{' '}
-</div>))
+  </div>
+))
 
 const rtlLanguages = ['ar']
 
-export function PreferenceComponent() {
-    const [preference, setPreference] = useState(localStorage.getItem('lng'))
-    const [languages] = useState(providedLanguages)
-    const [theme, setTheme] = useState('auto')
-    const {setDirection} = useDirection()
-    const {i18n} = useTranslation()
-    const [themeList, setThemeList] = useState([])
-    const {setColorScheme} = useMantineColorScheme({
-        keepTransitions: true
-    })
-    const [loading, setLoading] = useState(true)
+export function PreferenceComponent () {
+  const [preference, setPreference] = useState(localStorage.getItem('lng'))
+  const [languages] = useState(providedLanguages)
+  const [theme, setTheme] = useState('auto')
+  const { setDirection } = useDirection()
+  const { i18n } = useTranslation()
+  const [themeList, setThemeList] = useState([])
+  const { setColorScheme } = useMantineColorScheme({
+    keepTransitions: true
+  })
+  const [loading, setLoading] = useState(true)
 
-    useEffect(() => {
-        const lng = localStorage.getItem('lng') || 'en'
-        setThemeList(themeData)
-        const _theme = localStorage.getItem('mantine-color-scheme-value') || 'auto'
-        setPreference(lng)
-        setTheme(_theme)
+  useEffect(() => {
+    const lng = localStorage.getItem('lng') || 'en'
+    setThemeList(themeData)
+    const _theme = localStorage.getItem('mantine-color-scheme-value') || 'auto'
+    setPreference(lng)
+    setTheme(_theme)
 
-        setLoading(false)
-    }, [])
+    setLoading(false)
+  }, [])
 
-    const changeLanguage = lng => {
-        i18n.changeLanguage(lng)
-        const direction = rtlLanguages.includes(lng) ? 'rtl' : 'ltr'
-        setDirection(direction)
-        localStorage.setItem('dir', direction)
-        localStorage.setItem('lng', lng)
+  const changeLanguage = lng => {
+    i18n.changeLanguage(lng)
+    const direction = rtlLanguages.includes(lng) ? 'rtl' : 'ltr'
+    setDirection(direction)
+    localStorage.setItem('dir', direction)
+    localStorage.setItem('lng', lng)
+  }
+
+  const handleLanguageChange = option => {
+    changeLanguage(option.value)
+  }
+
+  const handleColorSchemeChange = option => {
+    const _theme = option
+      ? option
+      : localStorage.getItem('mantine-color-scheme-value') || 'auto'
+    if (_theme && _theme !== theme) {
+      setTheme(_theme)
+      setColorScheme(_theme)
+      localStorage.setItem('mantine-color-scheme-value', _theme)
     }
+  }
 
-    const handleLanguageChange = option => {
-        changeLanguage(option.value)
-    }
+  if (loading) {
+    return (
+      <Container
+        m={0}
+        p={0}
+        px={10}
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh'
+        }}
+      >
+        {' '}
+        <Loader />{' '}
+      </Container>
+    )
+  }
 
-    const handleColorSchemeChange = option => {
-        const _theme = option ? option : localStorage.getItem('mantine-color-scheme-value') || 'auto';
-        if (_theme && _theme !== theme) {
-            setTheme(_theme);
-            setColorScheme(_theme);
-            localStorage.setItem('mantine-color-scheme-value', _theme);
-        }
-    }
+  return (
+    <Container m={0} p={0} px={10}>
+      <Grid align='center' m={0} p={0}>
+        <Grid.Col span={{ base: 6, sm: 6, md: 6, lg: 6, xl: 6 }}>
+          <Text size='sm'>Theme</Text>
+        </Grid.Col>
+        <Grid.Col span={{ base: 6, sm: 6, md: 6, lg: 6, xl: 6 }}>
+          <SegmentedControl
+            data={themeData.map(item => ({
+              value: item.value,
+              label: (
+                <div className='flex items-center justify-center gap-2'>
+                  {createElement(item.icon, { size: 14 })}
+                  {item.label}
+                </div>
+              )
+            }))}
+            withItemsBorders
+            onChange={handleColorSchemeChange}
+            value={theme}
+            fullWidth
+            radius={'sm'}
+            transitionDuration={300}
+            transitionTimingFunction='linear'
+          />
+        </Grid.Col>
 
-    if (loading) {
-        return (<Container
-            m={0}
-            p={0}
-            px={10}
-            style={{
-                display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'
-            }}
-        >
-            {' '}
-            <Loader/>{' '}
-        </Container>)
-    }
-
-    return (<Container m={0} p={0} px={10}>
-        <Grid align='center' m={0} p={0}>
-            <Grid.Col span={{base: 6, sm: 6, md: 6, lg: 6, xl: 6}}>
-                <Text size='sm'>Theme</Text>
-            </Grid.Col>
-            <Grid.Col span={{base: 6, sm: 6, md: 6, lg: 6, xl: 6}}>
-                <Select
-                    data={themeList.map(item => ({...item, component: SelectItem}))}
-                    onChange={handleColorSchemeChange}
-                    defaultValue={theme}
-                    allowDeselect={false}
-                    checkIconPosition="right"
-                    styles={{
-                        item: {
-                            display: 'flex', alignItems: 'center', justifyContent: 'space-between',
-                        }
-                    }}
-                />
-            </Grid.Col>
-
-            <Grid.Col span={{base: 6, sm: 6, md: 6, lg: 6, xl: 6}}>
-                <Text size='sm'>Language</Text>
-            </Grid.Col>
-            <Grid.Col span={{base: 6, sm: 6, md: 6, lg: 6, xl: 6}}>
-                <Select
-                    size='sm'
-                    placeholder='Select System Language'
-                    data={languages}
-                    defaultValue={preference}
-                    checkIconPosition="right"
-                    allowDeselect={false}
-                    onChange={(_value, option) => handleLanguageChange(option)}
-                />
-            </Grid.Col>
-        </Grid>
-    </Container>)
+        <Grid.Col span={{ base: 6, sm: 6, md: 6, lg: 6, xl: 6 }}>
+          <Text size='sm'>Language</Text>
+        </Grid.Col>
+        <Grid.Col span={{ base: 6, sm: 6, md: 6, lg: 6, xl: 6 }}>
+          <Select
+            size='sm'
+            placeholder='Select System Language'
+            data={languages}
+            defaultValue={preference}
+            checkIconPosition='right'
+            allowDeselect={false}
+            onChange={(_value, option) => handleLanguageChange(option)}
+          />
+        </Grid.Col>
+      </Grid>
+    </Container>
+  )
 }
