728e9a77a62ed49210876339c36d1b579e32f52e
Imran <flexmindinnovations@outlook.com>
2024-11-18 09:42:07 +0530

    Update Preference.jsx

--- src/components/Preference.jsx
+++ src/components/Preference.jsx
@@ -3,37 +3,33 @@ import {useTranslation} from 'react-i18next'
 import {forwardRef, useEffect, useState} from 'react'
 import {MonitorDot, MoonIcon, Sun} from 'lucide-react'
 
-const providedLanguages = [
-    {id: 1, label: 'English', value: 'en'},
-    {id: 2, label: 'Arabic', value: 'ar'}
-]
+const providedLanguages = [{id: 1, label: 'English', value: 'en'}, {id: 2, label: 'Arabic', value: 'ar'}]
 
-const themeData = [
-    {value: 'auto', label: 'Auto', icon: MonitorDot},
-    {value: 'light', label: 'Light', icon: Sun},
-    {value: 'dark', label: 'Dark', icon: MoonIcon}
-]
+const themeData = [{value: 'auto', label: 'Auto', icon: MonitorDot}, {
+    value: 'light',
+    label: 'Light',
+    icon: Sun
+}, {value: 'dark', label: 'Dark', icon: MoonIcon}]
 
-const SelectItem = forwardRef(({label, icon, ...others}, ref) => (
-    <div ref={ref} {...others}>
+// eslint-disable-next-line react/display-name
+const SelectItem = forwardRef(({label, icon, ...others}, ref) => (<div ref={ref} {...others}>
+    {' '}
+    <Group noWrap>
         {' '}
-        <Group noWrap>
-            {' '}
-            {icon} <Text>{label}</Text>{' '}
-        </Group>{' '}
-    </div>
-))
+        {icon} <Text>{label}</Text>{' '}
+    </Group>{' '}
+</div>))
 
 const rtlLanguages = ['ar']
 
 export function PreferenceComponent() {
     const [preference, setPreference] = useState(localStorage.getItem('lng'))
-    const [languages, setLanguages] = useState(providedLanguages)
+    const [languages] = useState(providedLanguages)
     const [theme, setTheme] = useState('auto')
-    const {dir, setDirection} = useDirection()
+    const {setDirection} = useDirection()
     const {i18n} = useTranslation()
-    const [themeList, setThemeList] = useState()
-    const {colorScheme, setColorScheme} = useMantineColorScheme({
+    const [themeList, setThemeList] = useState([])
+    const {setColorScheme} = useMantineColorScheme({
         keepTransitions: true
     })
     const [loading, setLoading] = useState(true)
@@ -70,66 +66,53 @@ export function PreferenceComponent() {
     }
 
     if (loading) {
-        return (
-            <Container
-                m={0}
-                p={0}
-                px={10}
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    height: '100vh'
-                }}
-            >
-                {' '}
-                <Loader/>{' '}
-            </Container>
-        )
+        return (<Container
+            m={0}
+            p={0}
+            px={10}
+            style={{
+                display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'
+            }}
+        >
+            {' '}
+            <Loader/>{' '}
+        </Container>)
     }
 
-    return (
-        <Container m={0} p={0} px={10}>
-            <Grid align='center' m={0} p={0}>
-                <Grid.Col span={{base: 6, sm: 6, md: 6, lg: 6, xl: 6}}>
-                    <Text size='sm'>Theme</Text>
-                </Grid.Col>
-                <Grid.Col span={{base: 6, sm: 6, md: 6, lg: 6, xl: 6}}>
-                    <Select
-                        data={themeList.map(item => ({...item, component: SelectItem}))}
-                        onChange={handleColorSchemeChange}
-                        defaultValue={theme}
-                        allowDeselect={false}
-                        styles={{
-                            item: {
-                                display: 'flex',
-                                alignItems: 'center',
-                                justifyContent: 'space-between', // Align label and tick on opposite sides
-                            },
-                            selected: {
-                                justifyContent: 'space-between', // Ensure the selected tick is moved to the right
-                            },
-                            itemIcon: {
-                                marginRight: 10, // Optional: Adjust spacing between icon and label
-                            }
-                        }}
-                    />
-                </Grid.Col>
+    return (<Container m={0} p={0} px={10}>
+        <Grid align='center' m={0} p={0}>
+            <Grid.Col span={{base: 6, sm: 6, md: 6, lg: 6, xl: 6}}>
+                <Text size='sm'>Theme</Text>
+            </Grid.Col>
+            <Grid.Col span={{base: 6, sm: 6, md: 6, lg: 6, xl: 6}}>
+                <Select
+                    data={themeList.map(item => ({...item, component: SelectItem}))}
+                    onChange={handleColorSchemeChange}
+                    defaultValue={theme}
+                    allowDeselect={false}
+                    checkIconPosition="right"
+                    styles={{
+                        item: {
+                            display: 'flex', alignItems: 'center', justifyContent: 'space-between',
+                        }
+                    }}
+                />
+            </Grid.Col>
 
-                <Grid.Col span={{base: 6, sm: 6, md: 6, lg: 6, xl: 6}}>
-                    <Text size='sm'>Language</Text>
-                </Grid.Col>
-                <Grid.Col span={{base: 6, sm: 6, md: 6, lg: 6, xl: 6}}>
-                    <Select
-                        size='sm'
-                        placeholder='Select System Language'
-                        data={languages}
-                        defaultValue={preference}
-                        allowDeselect={false}
-                        onChange={(_value, option) => handleLanguageChange(option)}
-                    />
-                </Grid.Col>
-            </Grid>
-        </Container>
-    )
+            <Grid.Col span={{base: 6, sm: 6, md: 6, lg: 6, xl: 6}}>
+                <Text size='sm'>Language</Text>
+            </Grid.Col>
+            <Grid.Col span={{base: 6, sm: 6, md: 6, lg: 6, xl: 6}}>
+                <Select
+                    size='sm'
+                    placeholder='Select System Language'
+                    data={languages}
+                    defaultValue={preference}
+                    checkIconPosition="right"
+                    allowDeselect={false}
+                    onChange={(_value, option) => handleLanguageChange(option)}
+                />
+            </Grid.Col>
+        </Grid>
+    </Container>)
 }
