d0d984f07808f2aaa2f3bcd816d3eacaa4cb6bd2
shafiquddin <shafiquddin2k@gmail.com>
2025-02-23 15:48:19 +0530

    prescription works

--- src/contexts/ApiConfigContext.jsx
+++ src/contexts/ApiConfigContext.jsx
@@ -1,104 +1,122 @@
-import {createContext, useContext} from 'react'
-import {useDirection} from '@mantine/core'
-import {useTranslation} from 'react-i18next'
+import { createContext, useContext } from "react";
+import { useDirection } from "@mantine/core";
+import { useTranslation } from "react-i18next";
 
 const defaultApiConfig = {
-    appConfig: {
-        opacity: '0.4'
-    },
-    translate: `http://localhost:5000/translate`,
-    auth: {
-        login: `/User/SignIn`,
-        register: ``,
-        getUserDetails: ``
-    },
-    admin: {
-        getAdminInfoById: (id) => `/Admin/getAdminInfoById/${id}`
-    },
-    clients: {
-        getList: `/ClientPersonalInfo/getClientPersonalInfoList`,
-        updateUserImage: (id) => `/ClientPersonalInfo/UpdateProfileImage?ClientId=${id}`,
-        save: `/ClientPersonalInfo/saveClientPersonalInfo`,
-        update: (id) => `/ClientPersonalInfo/updateClientPersonalInfo/${id}`
-    },
-    doctors: {
-        getList: `/Doctor/getDoctorList`,
-        getDoctorInfoById: (id) => `/Doctor/getDoctorInfoById/${id}`,
-        getAvailability: `/DoctorTiming/GetDoctorTimingList`,
-        getAvailabilityById: (doctorId) => `/DoctorTiming/GetDoctorTimingByDoctorId/${doctorId}`,
-        updateAvailability: (doctorTimingId) => `/DoctorTiming/updateDoctorTiming/${doctorTimingId}`,
-        updateDoctorImage: (id) => `/Doctor/UpdateDoctorProfileImage?doctorId=${id}`,
-        save: `/Doctor/SignUp`,
-        getDoctorTimingByDoctorIdAndDayWise: (doctorId, dayName) => `/DoctorTiming/GetDoctorTimingByDoctorIdAndDayWise?doctorId=${doctorId}&dayName=${dayName}`,
-        saveDoctorAvailability: `/DoctorTiming/saveDoctorTiming`,
-        updateDoctorAvailability: `DoctorTiming/updateDoctorTiming`,
-        getTimeSlots: (doctorId, dayOfWeek, requestedDate) => `/DoctorTiming/GetTimeSlots/${doctorId}/${dayOfWeek}/${requestedDate}`,
-        update: (id) => `/Doctor/UpdateDoctorPersonalInfo/${id}`
-    },
-    patientVisits: {
-        getList: `/PatientVisit/GetPatientVisitList`,
-        getPatientVisitInfoByVisitId: (id) => `/api/PatientVisit/getPatientVisitInfoByVisitId/${id}`,
-        savePatientVisit: `/PatientVisit/SavePatientVisit`,
-        updatePatientVisit: (visitId) => `/api/PatientVisit/updatePatientVisit/${visitId}`
-    },
-    testTypes: {
-        getList: `/TestType/getTestTypeList`
-    },
-    document: {
-        getList: `/DocumentType/GetDocumentTypeList`,
-        saveDocument: `/DocumentType/saveDocumentType`,
-        updateDocument: documentId =>
-            `/DocumentType/updateDocumentType/${documentId}`,
-        deleteDocument: documentId => `/DocumentType/deleteDocument/${documentId}`
-    },
-    appointment: {
-        getList: `/Appointment/GetAppointmentList`,
-        getAppointmentDetailsById: (appointmentId) => `/Appointment/GetAppointmentDetailsById/${appointmentId}`,
-        getAppointmentListByPatientIdId: (patientId) => `/Appointment/GetAppointmentListByPatientIdWise/${patientId}`,
-        getAppointmentListByDoctorId: (doctorId) => `/Appointment/GetAppointmentListByDoctorIdWise/${doctorId}`,
-        bookAppointment: `/Appointment/BookAppointment`,
-    }
-}
+  appConfig: {
+    opacity: "0.4",
+  },
+  translate: `http://localhost:5000/translate`,
+  auth: {
+    login: `/User/SignIn`,
+    register: ``,
+    getUserDetails: ``,
+  },
+  admin: {
+    getAdminInfoById: (id) => `/Admin/getAdminInfoById/${id}`,
+  },
+  clients: {
+    getList: `/ClientPersonalInfo/getClientPersonalInfoList`,
+    updateUserImage: (id) =>
+      `/ClientPersonalInfo/UpdateProfileImage?ClientId=${id}`,
+    save: `/ClientPersonalInfo/saveClientPersonalInfo`,
+    update: (id) => `/ClientPersonalInfo/updateClientPersonalInfo/${id}`,
+  },
+  doctors: {
+    getList: `/Doctor/getDoctorList`,
+    getDoctorInfoById: (id) => `/Doctor/getDoctorInfoById/${id}`,
+    getAvailability: `/DoctorTiming/GetDoctorTimingList`,
+    getAvailabilityById: (doctorId) =>
+      `/DoctorTiming/GetDoctorTimingByDoctorId/${doctorId}`,
+    updateAvailability: (doctorTimingId) =>
+      `/DoctorTiming/updateDoctorTiming/${doctorTimingId}`,
+    updateDoctorImage: (id) =>
+      `/Doctor/UpdateDoctorProfileImage?doctorId=${id}`,
+    save: `/Doctor/SignUp`,
+    getDoctorTimingByDoctorIdAndDayWise: (doctorId, dayName) =>
+      `/DoctorTiming/GetDoctorTimingByDoctorIdAndDayWise?doctorId=${doctorId}&dayName=${dayName}`,
+    saveDoctorAvailability: `/DoctorTiming/saveDoctorTiming`,
+    updateDoctorAvailability: `DoctorTiming/updateDoctorTiming`,
+    getTimeSlots: (doctorId, dayOfWeek, requestedDate) =>
+      `/DoctorTiming/GetTimeSlots/${doctorId}/${dayOfWeek}/${requestedDate}`,
+    update: (id) => `/Doctor/UpdateDoctorPersonalInfo/${id}`,
+  },
+  patientVisits: {
+    getList: `/PatientVisit/GetPatientVisitList`,
+    getPatientVisitInfoByVisitId: (id) =>
+      `/api/PatientVisit/getPatientVisitInfoByVisitId/${id}`,
+    savePatientVisit: `/PatientVisit/SavePatientVisit`,
+    updatePatientVisit: (visitId) =>
+      `/api/PatientVisit/updatePatientVisit/${visitId}`,
+  },
+  testTypes: {
+    getList: `/TestType/getTestTypeList`,
+  },
+  document: {
+    getList: `/DocumentType/GetDocumentTypeList`,
+    saveDocument: `/DocumentType/saveDocumentType`,
+    updateDocument: (documentId) =>
+      `/DocumentType/updateDocumentType/${documentId}`,
+    deleteDocument: (documentId) =>
+      `/DocumentType/deleteDocument/${documentId}`,
+  },
+  appointment: {
+    getList: `/Appointment/GetAppointmentList`,
+    getAppointmentDetailsById: (appointmentId) =>
+      `/Appointment/GetAppointmentDetailsById/${appointmentId}`,
+    getAppointmentListByPatientIdId: (patientId) =>
+      `/Appointment/GetAppointmentListByPatientIdWise/${patientId}`,
+    getAppointmentListByDoctorId: (doctorId) =>
+      `/Appointment/GetAppointmentListByDoctorIdWise/${doctorId}`,
+    bookAppointment: `/Appointment/BookAppointment`,
+  },
+  medicine: {
+    getMedicineList: (pageNumber, pageSize, searchTerm) =>
+      `/Medicine/getMedicineList?pageNumber=${pageNumber}&pageSize=${pageSize}&searchTerm=${searchTerm}`,
+  },
+  DoctorPrescription: {
+    saveDoctorPrescription: `/DoctorPrescription/saveDoctorPrescription`,
+  },
+};
 
 const ApiConfigContext = createContext({
-    apiConfig: defaultApiConfig,
-    setPreferences: () => {
+  apiConfig: defaultApiConfig,
+  setPreferences: () => {},
+});
+
+export const ApiConfigProvider = ({ children }) => {
+  const { setDirection } = useDirection();
+  const { i18n } = useTranslation();
+
+  const setPreferences = () => {
+    let dir = localStorage.getItem("dir");
+    let lng = localStorage.getItem("lng");
+    if (!dir) {
+      dir = "ltr";
+      localStorage.setItem("dir", dir);
     }
-})
-
-export const ApiConfigProvider = ({children}) => {
-    const {setDirection} = useDirection()
-    const {i18n} = useTranslation()
-
-    const setPreferences = () => {
-        let dir = localStorage.getItem('dir')
-        let lng = localStorage.getItem('lng')
-        if (!dir) {
-            dir = 'ltr'
-            localStorage.setItem('dir', dir)
-        }
-        if (!lng) {
-            lng = 'en'
-            localStorage.setItem('lng', lng)
-        }
-
-        setDirection(dir)
-        i18n.changeLanguage(lng)
+    if (!lng) {
+      lng = "en";
+      localStorage.setItem("lng", lng);
     }
 
-    return (
-        <ApiConfigContext.Provider
-            value={{apiConfig: defaultApiConfig, setPreferences}}
-        >
-            {children}
-        </ApiConfigContext.Provider>
-    )
-}
+    setDirection(dir);
+    i18n.changeLanguage(lng);
+  };
+
+  return (
+    <ApiConfigContext.Provider
+      value={{ apiConfig: defaultApiConfig, setPreferences }}
+    >
+      {children}
+    </ApiConfigContext.Provider>
+  );
+};
 
 export const useApiConfig = () => {
-    const context = useContext(ApiConfigContext)
-    if (!context) {
-        throw new Error('useApiConfig must be used within an ApiConfigProvider')
-    }
-    return context
-}
+  const context = useContext(ApiConfigContext);
+  if (!context) {
+    throw new Error("useApiConfig must be used within an ApiConfigProvider");
+  }
+  return context;
+};
--- src/hooks/AddEditModal.jsx
+++ src/hooks/AddEditModal.jsx
@@ -1,43 +1,53 @@
-import {useCallback} from 'react';
-import {modals} from "@mantine/modals";
-import {useTranslation} from "react-i18next";
+import { useCallback } from "react";
+import { modals } from "@mantine/modals";
+import { useTranslation } from "react-i18next";
 
 export function useModal(handleRefresh) {
-    const {t} = useTranslation();
-    const openModal = useCallback((
-        {
-            Component, data = [] | {} | null, mode = 'add', title = 'Add ',
-            handleRefresh,
-            props,
-            isAddEdit = true,
-            size = 'lg'
-        }
-    ) => {
-        modals.closeAll();
-        modals.open({
-            title: `${isAddEdit ? mode === "edit" ? t("update") : t("add") : ''}` + ` ${title}`,
-            centered: true,
-            trapFocus: false,
-            size: size,
-            classNames: {
-                body: `${props} !p-0 !overflow-hidden`,
-            },
-            styles: {
-                title: {fontWeight: '500', fontSize: '14px'},
-            },
-            children: (
-                <Component
-                    mode={mode}
-                    data={data}
-                    handleCancel={(event) => {
-                        const {refresh} = event;
-                        modals.closeAll();
-                        refresh && handleRefresh({refresh});
-                    }}
-                />
-            ),
-        });
-    }, [handleRefresh])
+  const { t } = useTranslation();
+  const openModal = useCallback(
+    ({
+      Component,
+      data = [] | {} | null,
+      mode = "add",
+      title = "Add ",
+      handleRefresh,
+      props,
+      isAddEdit = true,
+      size = "lg",
+    }) => {
+      modals.closeAll();
+      modals.open({
+        title:
+          `${isAddEdit ? (mode === "edit" ? t("update") : t("add")) : ""}` +
+          ` ${title}`,
+        centered: true,
+        trapFocus: false,
+        size: size,
+        classNames: {
+          body: `${props} !p-0 !overflow-hidden`,
+        },
+        styles: {
+          title: { fontWeight: "500", fontSize: "14px" },
+        },
+        children: (
+          <Component
+            mode={mode}
+            data={data}
+            handleCancel={(event) => {
+              const { refresh } = event;
+              modals.closeAll();
+              refresh && handleRefresh({ refresh });
+            }}
+          />
+        ),
+      });
+    },
+    [handleRefresh]
+  );
+  // Add closeModal function
+  const closeModal = () => {
+    modals.closeAll();
+  };
 
-    return {openModal};
+  return { openModal, closeModal };
 }
